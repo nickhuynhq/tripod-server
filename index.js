7f069d38eb489aa14a3900365a5b1aed8ac17121
Nicholas Huynh <nickhuynhq@gmail.com>
2022-11-14 20:31:43 -0500

    Have routes declared after CORS so requests can be made

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ import postRoutes from "./routes/posts.js";
 
 const app = express();
 
-app.use("/posts", postRoutes);
-
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+app.use("/posts", postRoutes);
 
 const CONNECTION_URL =
   `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.hpz45iv.mongodb.net/?retryWrites=true&w=majority`;
