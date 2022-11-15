57e63e85e7f9c8ee84fdd3f0958b822630fe4119
Nicholas Huynh <nickhuynhq@gmail.com>
2022-11-15 13:57:44 -0500

    Fix issue with id not defined in deletePost controller

--- controllers/posts.js
+++ controllers/posts.js
@@ -18,9 +18,9 @@ export const createPost = async (req, res) => {
   try {
     await newPost.save();
 
-    res.status(201).json(newPost)
+    res.status(201).json(newPost);
   } catch (error) {
-    res.status(409).json({message: error})
+    res.status(409).json({ message: error });
   }
 };
 
@@ -28,19 +28,25 @@ export const updatePost = async (req, res) => {
   const { id: _id } = req.params;
   const post = req.body;
 
-  if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id')
+  if (!mongoose.Types.ObjectId.isValid(_id))
+    return res.status(404).send("No post with that id");
 
-  const updatedPost = await PostMessage.findByIdAndUpdate(_id, {...post, _id}, { new: true })
+  const updatedPost = await PostMessage.findByIdAndUpdate(
+    _id,
+    { ...post, _id },
+    { new: true }
+  );
 
   res.json(updatedPost);
-}
+};
 
 export const deletePost = async (req, res) => {
-  const { id: _id } = req.params;
+  const { id } = req.params;
 
-  if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id')
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send("No post with that id");
 
   await PostMessage.findByIdAndDelete(id);
 
-  res.json({message: 'Post deleted successfully'})
-}
+  res.json({ message: "Post deleted successfully" });
+};
