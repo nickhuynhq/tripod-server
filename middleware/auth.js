925531fd3725fa86e02fc41ebd847f0c4abd1f1c
Nicholas Huynh <nickhuynhq@gmail.com>
2022-11-16 20:20:41 -0500

    Create auth middleware to be used in routes

---
+++ middleware/auth.js
@@ -0,0 +1,30 @@
+import jwt from "jsonwebtoken";
+
+// User wants to like a post
+// User clicks like button => auth middleware (next) => like controller
+
+const auth = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    // Check if token is Google Auth or my Custom Auth
+    const isCustomAuth = token.length < 500;
+
+    let decodedData;
+
+    if (token && isCustomAuth) {
+      decodedData = jwt.verify(token, "test");
+
+      req.userId = decodedData?.id;
+    } else {
+      decodedData = jwt.decode(token);
+
+      req.userId = decodedData?.sub;
+    }
+
+    next();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default auth;
