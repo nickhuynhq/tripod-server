74f5061dcd8559bab3d3094832b2cc5781877cb8
Nicholas Huynh <nickhuynhq@gmail.com>
2022-11-19 13:33:31 -0500

    Route and style Post Details component

--- controllers/posts.js
+++ controllers/posts.js
@@ -17,17 +17,18 @@ export const getPosts = async (req, res) => {
   }
 }
 
-export const getPost = async (req, res) => {
+export const getPost = async (req, res) => { 
   const { id } = req.params;
-  
+
   try {
       const post = await PostMessage.findById(id);
-
+      
       res.status(200).json(post);
-  } catch (error) {    
+  } catch (error) {
       res.status(404).json({ message: error });
   }
 }
+
 
 export const getPostsBySearch = async (req, res) => {
   const { searchQuery, tags } = req.query;
